355eb155c250a220705cf8fbd80b02e81ce10548
Avadh Dhamsaniya <avadh.dhamsaniya@tatvasoft.com>
2019-12-05 11:37:12 +0530

    Add checkbox in user list

--- Demo/Demo/ClientApp/src/components/UserList.js
+++ Demo/Demo/ClientApp/src/components/UserList.js
@@ -1,4 +1,5 @@
 ﻿import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 import axios from 'axios';
 
 const $ = require('jquery');
@@ -6,16 +7,32 @@ $.DataTable = require('datatables.net');
 
 export default class UserList extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            reload: false
+        }
+        this.deleteUser = this.deleteUser.bind(this);
+    }
+
     async componentDidMount() {
         let res = await axios.get("http://192.168.2.44/Api/Employee/AllEmployeeDetails");
         $(this.refs.main).DataTable({
             dom: '<"top"l>rt<"bottom"ip><"clear">',
             data: res.data,
             columns: [
-                { data: "userId", title: "UserId", className: "display-none" },
-                { data: "email", title: "Email Address" },
+                {
+                    title: "<input type='checkbox' className='togBtn' id='selectall' ref='selectall' name='selectall'>", data: "userId",
+                    //render: function (data, type, row) {
+                    //    return "<input type='checkbox' class='togBtn' onChange=\"selectCheckbox(this);\" id= " + row.userId + ">";
+                    //}
+                    createdCell: (td, cellData, rowData, row, col) => {
+                        ReactDOM.render(<input type='checkbox' className='togBtn' id={rowData.userId} />, td);
+                    }
+                },
                 { data: "first_name", title: "First Name" },
                 { data: "last_name", title: "Last Name" },
+                { data: "email", title: "Email Address" },
                 { data: "mobile_Number", title: "Contact Number" },
                 { data: "education_Name", title: "Education" },
                 { data: "salary", title: "Salary" },
@@ -37,7 +54,7 @@ export default class UserList extends Component {
             paging: true,
             pageLength: 10,
             pagingType: 'numbers',
-            order: [[0, "asc"]],
+            order: [[1, "asc"]],
             language: {
                 lengthMenu: "Show _MENU_  entries",
                 info: "Showing _START_ to _END_ of _TOTAL_ entries",
@@ -51,7 +68,7 @@ export default class UserList extends Component {
             emptyTable: "No Record Found",
             lengthMenu: [10, 20, 30, 40, 50],
             columnDefs: [
-                { "targets": [0, 2, 3, 4], "orderable": false }
+                { "targets": [0, 4,6,7,10], "orderable": false }
             ],
             initComplete: function (setting, json) {
             },
@@ -66,7 +83,7 @@ export default class UserList extends Component {
                 else {
                     $('#main_info').show();
                     $('#main_paginate').show();
-                    $('#glexchangebal-setting-table_length').show();
+                    $('#main_length').show();
                 }
                 $('.dataTables_info').addClass('showing-total-entries table-bottom pull-left');
                 $('.dataTables_paginate').addClass('table-bottom-links pull-right');
@@ -76,6 +93,21 @@ export default class UserList extends Component {
                 } else {
                     pgr.show();
                 }
+
+                $('#selectall').on("change", function (e) {
+                    $("#main").find('td > input').prop('checked', $(this).prop('checked'));
+                });
+
+                $("#main").find('td > input').on("change", function (e) {
+                    if (!$(this).is(':checked')) {
+                        $('#main').find('th > input:checkbox').prop('checked', false);
+                    }
+                    else {
+                        if ($('#main').find('td >input:checkbox').length == $('#main').find('td >input:checkbox:checked').length) {
+                            $('#main').find('th > input:checkbox').prop('checked', true);
+                        }
+                    }
+                });
             }
         });
     }
@@ -87,15 +119,33 @@ export default class UserList extends Component {
             .destroy(true);
     }
 
-    shouldComponentUpdate() {
-        return false;
+     deleteUser(e) {
+        var table = this.refs.main;
+        var inputs = table.querySelectorAll("td input[type='checkbox']");
+        let userIds = [];
+        for (var i = 0; i < inputs.length; i++) {
+            if (inputs[i].checked) {
+                userIds = [...userIds, inputs[i].id];
+            }
+        }
+        let res =  axios.delete("http://192.168.2.44/Api/Employee/DeleteEmployee?" + userIds.toString());
+        if (res.status == 200) {
+            this.setState({
+                reload: true
+            });
+        }
     }
 
     render() {
         return (
-            <div className="table-resposive">
-                <table ref="main" id="main" className="darkgrid table table-bordered table-hover">
-                </table>
+            <div>
+                <div className="button-holder">
+                    <button className="cus-button primary" style={{ "marginLeft": "15px", "float": "right" }} onClick={this.deleteUser} >Delete</button>
+                </div>
+                <div className="table-resposive">
+                    <table ref="main" id="main" className="darkgrid table table-bordered table-hover">
+                    </table>
+                </div>
             </div>
         )
     }
